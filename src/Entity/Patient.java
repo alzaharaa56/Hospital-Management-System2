b62429d6091b92b712478c6b649caa4de5d61871
alzahara <shahdaljabri33@gmail.com>
2026-05-10 12:09:18 +0400

    Task 1.2: Create Patient Class (extends Person)

--- src/Entity/Patient.java
+++ src/Entity/Patient.java
@@ -1,4 +1,54 @@
 package Entity;
 
-public class Patient {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patient extends Person {
+    private String patientId;
+    private String bloodGroup;
+    private List<String> allergies;
+    private String emergencyContact;
+    private LocalDate registrationDate;
+    private String insuranceId;
+    private List<MedicalRecord> medicalRecords;
+    private List<Appointment> appointments;
+
+    public Patient(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                   String gender, String phoneNumber, String email, String address,
+                   String patientId, String bloodGroup, String emergencyContact,
+                   LocalDate registrationDate, String insuranceId) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+        this.patientId = patientId;
+        this.bloodGroup = bloodGroup;
+        this.emergencyContact = emergencyContact;
+        this.registrationDate = registrationDate;
+        this.insuranceId = insuranceId;
+        this.allergies = new ArrayList<>();
+        this.medicalRecords = new ArrayList<>();
+        this.appointments = new ArrayList<>();
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Patient ID: " + patientId);
+        System.out.println("Blood Group: " + bloodGroup);
+        System.out.println("Emergency Contact: " + emergencyContact);
+        System.out.println("Registration Date: " + registrationDate);
+        System.out.println("Insurance ID: " + insuranceId);
+    }
+
+    public void addMedicalRecord(MedicalRecord record) {
+        medicalRecords.add(record);
+    }
+
+    public void addAppointment(Appointment appointment) {
+        appointments.add(appointment);
+    }
+
+    public void updateInsurance(String newInsuranceId) {
+        this.insuranceId = newInsuranceId;
+    }
 }
+
