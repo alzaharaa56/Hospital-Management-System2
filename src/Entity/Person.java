498d8113d2deb120487496755e46944be5355e62
alzahara <shahdaljabri33@gmail.com>
2026-05-10 12:07:56 +0400

    Task 1.1: Create Base Person Class and Inheritance Hierarchy

---
+++ src/Entity/Person.java
@@ -0,0 +1,75 @@
+package Entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Person {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+
+    public Person(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                  String gender, String phoneNumber, String email, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public LocalDate getDateOfBirth() { return dateOfBirth; }
+    public void setDateOfBirth(LocalDate dateOfBirth) { this.dateOfBirth = dateOfBirth; }
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+
+    public void displayInfo() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("DOB: " + dateOfBirth);
+        System.out.println("Gender: " + gender);
+        System.out.println("Phone: " + phoneNumber);
+        System.out.println("Email: " + email);
+        System.out.println("Address: " + address);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " (" + id + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
